e5f60b5eb5a4a95f96d173a74f993ae4d2279265
Anaholcman <127428150+Anaholcman@users.noreply.github.com>
2023-08-14 16:36:56 +0000

    module city y quality

---
+++ ModuleCity.hs
@@ -0,0 +1,11 @@
+module City ( City, newC, nameC, distanceC )
+   where
+
+data City = Cit String Point deriving (Eq, Show)
+
+newC :: String -> Point -> City
+newC name location = Cit name location
+nameC :: City -> String
+nameC Cit name location = name 
+distanceC :: City -> City -> Float
+distanceC (Cit name1 location1) (Cit name2 location2) = difP location1 location2
