fd02de83cdfd644399c1db7afb0cca175b3e8386
Eliana Ostro <elianaostro@gmail.com>
2023-08-23 20:35:47 -0300

    cambie un toque de tunel y lo testie

--- Tunel.hs
+++ Tunel.hs
@@ -5,27 +5,31 @@ import Point (Point, newP, difP)
 import Quality (Quality, newQ, capacityQ, delayQ )
 import City ( City, newC, nameC, distanceC )
 import Link ( Link, newL, linksL, connectsL, capacityL, delayL )
+import GHC.Exts.Heap (GenClosure(link))
 
 data Tunel = Tun [Link] deriving (Eq, Show)
 
 newT :: [Link] -> Tunel
-newT [links] = Tun [links]
-
+newT = Tun
 
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
 connectsT city1 city2 (Tun tunnel) = (esExtremo city1 tunnel) && (esExtremo city2 tunnel)
 --{ dadas dos ciudades esta función da si si las ciudades son los extremos del túnel }
 
-esExtremo :: City -> [ Links ] -> Bool
-esExtremo city links = foldr (\link fold -> (connectada link && not fold)|| (not (connectada link) && fold)) false links
+esExtremo :: City -> [ Link ] -> Bool
+esExtremo city = foldr (\link fold -> (connectada link && not fold)|| (not (connectada link) && fold)) False
    where connectada = connectsL city
 
-usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
-usesT link [] = False
-usesT link (tunnel : links) 
-   | link == tunnel = True
-   | otherwise = usesT link links --no se si en este caso hay que poner = tunnel tunnels
+-- usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
+-- usesT link [] = False
+-- usesT link (tunnel : links) 
+--    | link == tunnel = True
+--    | otherwise = usesT link links --no se si en este caso hay que poner = tunnel tunnels
+
+-- no es mejor asi????
+usesT :: Link -> Tunel -> Bool -- indica si este tunel atraviesa ese link
+usesT link (Tun linksTun)= foldr ((||).(==link)) False linksTun
 
 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
-delayT links = foldr ( (+).delayL ) 0.0 links
+delayT (Tun linksTun) = foldr ( (+).delayL ) 0.0 linksTun
--- test.hs
+++ test.hs
@@ -3,14 +3,17 @@ import City ( City, newC, nameC, distanceC )
 import Quality ( Quality, newQ, capacityQ, delayQ )
 import Data.Binary (decode)
 import Link ( Link, newL, linksL, connectsL, capacityL, delayL )
--- import Tunel ( Tunel, newT, connectsT, usesT, delayT )      
+import Tunel ( Tunel, newT, connectsT, usesT, delayT )      
 
 punto1 = newP 5 5 
 punto2 = newP 1 2
 ciudad1 = newC "BsAs" punto1
 ciudad2 = newC "caba" punto2
+ciudad3 = newC "mardel" (newP 3 2)
 calidad = newQ "cobre" 5 1.5  
 link1 = newL ciudad1 ciudad2 calidad
+link2 = newL ciudad2 ciudad3 calidad
+tunel = newT [link1,link2]
 t = [ difP punto1 (newP 1 2) == 5.0 , 
       nameC ciudad1 == "BsAs"  ,
       distanceC ciudad1 ciudad2 == 5.0 , 
@@ -20,19 +23,9 @@ t = [ difP punto1 (newP 1 2) == 5.0 ,
       connectsL ciudad2 link1, 
       capacityL link1 == 5, 
       delayL link1 == 7.5 ,
+      not (connectsT ciudad1 ciudad2 tunel), 
+      connectsT ciudad1 ciudad3 tunel,
+      usesT link1 tunel, 
       True]
 
-
--- stickWith :: [Int] -> Stick
--- stickWith = foldr (\each fold -> push fold each) Vacio
-
--- initWith :: [Int] -> [Int] -> [Int] -> Hanoi
--- initWith i c d = Hanoi (stickWith i) (stickWith c) (stickWith d)
-
--- hanoi = initWith [] [1,3] [2]
-
--- t = [ pop ( Stack Vacio 2) == Vacio,
---       push Vacio 3 == Stack Vacio 3,
---       top (push Vacio 3) == 3,
---       stickWith [ 2, 3 ] == Stack (Stack Vacio 3) 2,
---       True ]
+-- delayT tunel
