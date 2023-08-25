4f4def6733800e3643045e0730782b3da41cba1a
Eliana Ostro <elianaostro@gmail.com>
2023-08-25 12:18:32 -0300

    cambios

--- Region.hs
+++ Region.hs
@@ -1,4 +1,4 @@
-module Region ( Region, newR, foundR, linkR, tunelR, pathR, linksForR, connectedR, linkedR, delayR, availableCapacityForR, usedCapacityForR )
+module Region ( Region, newR, foundR, linkR, tunelR, connectedR, linkedR, delayR, availableCapacityForR )
    where
 
 import Point ( Point, newP, difP)
@@ -13,27 +13,47 @@ newR :: Region
 newR = Reg [] [] []
 
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
-foundR (Reg citiesReg linksReg tunelsReg) newcity = if not (elem newcity citiesReg) then Reg (newcity:citiesRegs) linksReg tunelsReg else error "Ya existe esa ciudad"
+foundR (Reg citiesReg linksReg tunelsReg) newcity = if notElem newcity citiesReg then Reg ( newcity : citiesReg ) linksReg tunelsReg else error "Ya existe esa ciudad"
+--hay que ver que no este en el mismo lugar que otra ciudad
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
-linkR (Reg cities links tunels) city1 city2 calidad = Reg cities ((newL city1 city2 calidad):links) tunels --revisar q las citis esten en la region
+linkR (Reg citiesReg linksReg tunelsReg) city1 city2 calidad = Reg citiesReg ( newL city1 city2 calidad : linksReg) tunelsReg 
+--revisar q las citis esten en la region
 
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
-tunelR (Reg citiesR linksR tunels) citiesT = Reg citiesR links (newT (linksentre citiesT linksR):tunels)
+tunelR region@(Reg citiesReg linksReg tunelsReg) citiesT = Reg citiesReg linksReg (newT (linksentre region (ciudadespar citiesT)):tunelsReg)
 
-connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
 
-linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
-linkedR (Reg citiesReg linksReg tunelsReg) city1 city2 = foldr ((||).connectsL city1 city2) False linksReg
+{-hay que hacer esta funcion que cumpla:
+   -generar una lista ordenada de links con capacidad de soportar otro tunel entre las ciudades
+-}
+linksentre :: Region -> [ [ City ] ] -> [ Link ]
+linksentre region [] = []
 
-delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
+--linksentre region@(Reg citiesReg linksReg tunelsReg) cities = (\[city1 , city2] -> if linkedR region city1 city2 && (availableCapacityForR region city1 city2 > 0) then )
 
-availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
+linksentre region@(Reg citiesReg linksReg tunelsReg) ( city1 : ( city2 : cities)) = if linkedR region city1 city2 then [if linksL city1 city2 link then link | link <- linksReg] ++ (linksentre ( city2 : cities) region)
 
-linksentre :: [ City ] -> [ Link ] -> [ Link ]
-linksentre ( city1 : ( city2 : cities)) [] = error "primero hay que crear los links entre las ciudades"
-linksentre [] linksReg = []
-
+linksentre region@(Reg citiesReg linksReg tunelsReg) ( city1 : ( city2 : cities)) = foldr (\ link -> (if linksL city1 city2 link then link) ) 
 
 linksentre ( city1 : ( city2 : cities)) linksReg = [if linksL city1 city2 link then link | link <- linksReg] ++ (linksentre ( city2 : cities) linksReg)
+-------------------
 
+ciudadespar :: [ City ] -> [ [ City ] ]
+ciudadespar [ city1 , city2 ] = [[ city1 , city2 ]]
+ciudadespar ( city1 : ( city2 : cities)) = ([ city1 , city2 ] : ciudadespar ( city2 : cities))
+
+esvalido :: Region -> [ [ City ] ] -> Bool
+esvalido region cities = foldr (\[city1 , city2] -> (&&).(linkedR region city1 city2 && (availableCapacityForR region city1 city2 > 0))) True cities
+
+connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
+connectedR (Reg citiesReg linksReg tunelsReg) city1 city2 = foldr ((||).(connectsT city1 city2)) False tunelsReg
+
+linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
+linkedR (Reg citiesReg linksReg tunelsReg) city1 city2 = foldr ((||).(linksL city1 city2)) False linksReg
+
+delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
+-- suma del delay de cada link
+
+availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
+-- revisar todos los links existentes entre las ciudades, sumar la capacidad y restar la cantidad de tuneles que pasan
--- test.hs
+++ test.hs
@@ -4,6 +4,7 @@ import Quality ( Quality, newQ, capacityQ, delayQ )
 import Data.Binary (decode)
 import Link ( Link, newL, linksL, connectsL, capacityL, delayL )
 import Tunel ( Tunel, newT, connectsT, usesT, delayT )      
+import Region ( Region, newR, foundR, linkR, tunelR, connectedR, linkedR, delayR, availableCapacityForR )
 
 punto1 = newP 5 5 
 punto2 = newP 1 2
@@ -14,6 +15,8 @@ calidad = newQ "cobre" 5 1.5
 link1 = newL ciudad1 ciudad2 calidad
 link2 = newL ciudad2 ciudad3 calidad
 tunel = newT [link1,link2]
+argentina = 
+      
 t = [ difP punto1 (newP 1 2) == 5.0 , 
       nameC ciudad1 == "BsAs"  ,
       distanceC ciudad1 ciudad2 == 5.0 , 
@@ -28,4 +31,4 @@ t = [ difP punto1 (newP 1 2) == 5.0 ,
       usesT link1 tunel, 
       True]
 
--- delayT tunel
+
