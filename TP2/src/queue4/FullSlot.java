b440295a1bd4e00eafddcc7b2a14d33be6be2bd9
Eliana Ostro <elianaostro@gmail.com>
2023-09-13 19:30:30 -0300

    ultimo

---
+++ TP2/src/queue4/FullSlot.java
@@ -0,0 +1,18 @@
+package queue4;
+
+public class FullSlot extends Slots{
+	
+	public Object cargo;
+	public FullSlot(Object cargo) {
+		this.cargo = cargo;
+	}
+	
+	public Object ishead() {
+		return this.cargo;
+	}
+	
+	public boolean estavacio() {
+		return false;
+	}
+	
+}
