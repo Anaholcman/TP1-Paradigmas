e8fbe365f5587e71485d94ac923b7b2231b3dc9e
Ana Holcman <127428150+Anaholcman@users.noreply.github.com>
2023-09-17 12:45:39 -0300

    Delete TP2/src/queue4 directory

--- TP2/src/queue4/EmptySlot.java
+++
@@ -1,9 +0,0 @@
-package queue4;
-
-public class EmptySlot extends Slots {
-
-	public static String isempty = "Queue is empty";
-	public Object ishead() {	throw new Error(isempty); }
-	public boolean estavacio() {	return true;  }
-
-}
