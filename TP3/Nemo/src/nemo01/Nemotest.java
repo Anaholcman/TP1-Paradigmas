c51f45a011d799fb6b9450d1cbe7859a2febd795
Anaholcman <anitaholcman@gmail.com>
2023-10-19 11:44:58 -0300

    casi finished

--- TP3/Nemo/src/nemo01/Nemotest.java
+++ TP3/Nemo/src/nemo01/Nemotest.java
@@ -1,5 +1,6 @@
 package nemo01;
 
+import nemo01.depth.Deep;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,6 +15,7 @@ public class Nemotest {
     public void test02ComandoVacio() {
         Nemo nemo = new Nemo();
         nemo.indications ("");
+        createSubmarineWithCommand("");
         compareState( nemo, 0,true,0, 0, 1, 0);
     }
 
@@ -66,7 +68,7 @@ public class Nemotest {
     public void test09Girar90grados(){
         Nemo nemo = new Nemo();
         nemo.indications("ll"); // gira90grados
-        compareDirections(nemo, 0, -1);
+        compareDirections(nemo, -1, 0);
     }
 
     @Test
@@ -130,7 +132,26 @@ public class Nemotest {
         nemo.indications( "d d" );
         compareDepths(nemo, 2);
         compareCapacityOfLaunch(nemo, false);
-        assertThrowsLike ( ()->nemo.indications("m") , "No se puede liberar la capsula" );
+        assertThrowsLike ( ()->nemo.indications("m") , Deep.NoSePuedeLiberarLaCapsula );
+    }
+
+    @Test
+    public void test18DescenderyAvanzar() {
+        Nemo nemo = new Nemo();
+        nemo.indications( "df" );
+        compareDepths(nemo, 1);
+        //compareCapacityOfLaunch(nemo, true);  para mi no hace falta
+        compareCoords(nemo, 1, 0);
+    }
+
+    @Test
+    public void test19DescenderGirarYAvanzar(){
+        Nemo nemo = new Nemo();
+        nemo.indications( "drf" );
+        compareDepths(nemo, 1);
+        compareCapacityOfLaunch(nemo, true);
+        compareCoords(nemo, 0, 1);
+        compareDirections(nemo, 0, 1);
     }
 
     private static void compareState( Nemo nemo, int depth, boolean canLaunch,int xcoord, int ycoord, int xdire, int ydire) {
@@ -158,123 +179,10 @@ public class Nemotest {
         assertEquals ( message , assertThrows ( Exception.class , executable ) .getMessage() );
     }
 
-}
+    private Nemo createSubmarineWithCommand(String command){ ///// CHECK!!!!!
+        Nemo nemo = new Nemo();
+        nemo.indications(command);
+        return nemo;
+    }
 
-//public class Nemotest {
-//    @Test
-//    public void test00() {
-//        Nemo nemo = new Nemo();
-//        Cardinals north = new North();
-//        Coords coords = new Coords();
-//        assertEquals ( 0 , nemo.getDepth() );
-//        assertTrue ( nemo.InSurface() );
-//        assertTrue( nemo.getLocation().equals(coords) );
-//        assertTrue( nemo.getDirection().equals(north));
-//    }
-//    @Test
-//    public void test01() {
-//        Nemo nemo = new Nemo();
-//        Cardinals north = new North();
-//        Coords coords = new Coords();
-//        nemo.indications ("" );
-//        assertTrue( nemo.getLocation().equals(coords) );
-//        assertEquals ( nemo.getDepth() , 0 );
-//        assertTrue( nemo.getDirection().equals(north));
-//    }
-//
-//    @Test
-//    public void test02descender() {
-//        Nemo nemo = new Nemo();
-//        Cardinals north = new North();
-//        Coords coords = new Coords();
-//        nemo.indications ( "d" );
-//        assertTrue( nemo.getLocation().equals(coords) );
-//        assertEquals ( 1 , nemo.getDepth() );
-//        assertFalse ( nemo.InSurface() );
-//        assertTrue( nemo.getDirection().equals(north));
-//    }
-//
-//    @Test
-//    public void test03ascender() {
-//        Nemo nemo = new Nemo();
-//        Coords coords = new Coords();
-//        nemo.indications ("u" );
-//        assertTrue( nemo.getLocation().equals(coords) );
-//        assertTrue ( nemo.InSurface() );
-//    }
-//
-//    @Test
-//    public void test04rotarDerecha() {
-//        Nemo nemo = new Nemo();
-//        Cardinals east = new East() ;
-//        nemo.indications ( "r" );
-//        assertTrue( nemo.getDirection().equals(east));
-//    }
-//
-//    @Test
-//    public void test05rotarIzquierda() {
-//        Nemo nemo = new Nemo();
-//        Cardinals west = new West() ;
-//        nemo.indications ( "l" );
-//        assertTrue( nemo.getDirection().equals(west));
-//    }
-//
-//    @Test
-//    public void test06avanzar() {
-//        Nemo nemo = new Nemo();
-//        Cardinals north = new North();
-//        Coords coords = new Coords();
-//        coords.add( north );
-//        nemo.indications ( "f" );
-//        assertTrue( nemo.getLocation().equals(coords) );
-//        assertTrue( nemo.getDirection().equals(north));
-//    }
-//
-//    @Test
-//    public void test07avanzar2() {
-//        Nemo nemo = new Nemo();
-//        Cardinals north = new North();
-//        Coords coords = new Coords();
-//        coords.add( north );
-//        coords.add( north );
-//        nemo.indications ( "ff" );
-//        assertTrue( nemo.getLocation().equals(coords) );
-//        assertTrue( nemo.getDirection().equals(north));
-//    }
-//    @Test
-//    public void test08FallaTirarCapsula() {
-//        Nemo nemo = new Nemo();
-//        nemo.indications( "d d" );
-//        assertThrowsLike ( ()->nemo.indications("m") , "No se puede liberar la capsula" );
-//    }
-//
-//    @Test
-//    public void test09girar2veces(){
-//        Nemo nemo = new Nemo();
-//        Cardinals south = new South();
-//        nemo.indications("ll");
-//        assertTrue( nemo.direccion.equals(south));
-//    }
-//    @Test
-//    public void test09girarYVolver() {
-//        Nemo nemo = new Nemo();
-//        Cardinals north = new North();
-//        nemo.indications("lr");
-//        assertTrue( nemo.getDirection().equals(north));
-//    }
-//
-//
-//    @Test
-//    public void test10ascensoSinLimitacion() {
-//        Nemo nemo = new Nemo();
-//        nemo.indications ( "dddddd" );
-//        // Verificar que se pueda ascender múltiples unidades sin problemas.
-//        // queda en 0
-//        nemo.indications ( "uuuuuu" );
-//        assertEquals ( 0 , nemo.getDepth() );
-//    }
-//
-//    private void assertThrowsLike(Executable executable, String message) {
-//        assertEquals ( message , assertThrows ( Exception.class , executable ) .getMessage() );
-//    }
-//}
+}
--- TP3/Nemo/src/nemo01/depth/Deep.java
+++ TP3/Nemo/src/nemo01/depth/Deep.java
@@ -1,5 +1,7 @@
 package nemo01.depth;
 public class Deep extends DepthNavigator {
+    public static  String NoSePuedeLiberarLaCapsula = "No se puede liberar la capsula";
+
     public Deep(int profundidad, DepthNavigator anterior){
         this.profundidad = profundidad;
         this.anterior = anterior;
@@ -11,7 +13,7 @@ public class Deep extends DepthNavigator {
         return anterior;
     }
     public void capsula() {
-        throw new RuntimeException("No se puede liberar la capsula" );
+        throw new RuntimeException(NoSePuedeLiberarLaCapsula);
     }
     public boolean canLaunchCapsule() {
         return false;
