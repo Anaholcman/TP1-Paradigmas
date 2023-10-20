3debc8d90b316e2b23906ae8ddcdc875abe21efe
Anaholcman <anitaholcman@gmail.com>
2023-10-20 10:35:44 -0300

    tests terminados

--- TP3/Nemo/src/nemo01/Nemotest.java
+++ TP3/Nemo/src/nemo01/Nemotest.java
@@ -13,62 +13,49 @@ public class Nemotest {
 
     @Test
     public void test02ComandoVacio() {
-        Nemo nemo = new Nemo();
-        nemo.indications ("");
-        createSubmarineWithCommand("");
-        compareState( nemo, 0,true,0, 0, 1, 0);
+        compareState( createSubmarineWithCommand(""), 0,true,0, 0, 1, 0);
     }
 
     @Test
     public void test03Descender() {
-        Nemo nemo = new Nemo();
-        nemo.indications ( "d" );
+        Nemo nemo = createSubmarineWithCommand("d");
         compareDepths(nemo, 1);
         compareCapacityOfLaunch(nemo, true);
     }
 
     @Test
     public void test04AscenderDesdeSurface() {
-        Nemo nemo = new Nemo();
-        nemo.indications ("u" );
+        Nemo nemo = createSubmarineWithCommand("u");
         compareDepths(nemo, 0);
         compareCapacityOfLaunch(nemo, true);
     }
 
     @Test
     public void test05MultiplesAscensosDesdeSurface() {
-        Nemo nemo = new Nemo();
-        nemo.indications ( "uuuuu" );
+        Nemo nemo = createSubmarineWithCommand("uuuuu");
         compareDepths(nemo, 0);
         compareCapacityOfLaunch(nemo, true);
     }
     @Test
     public void test06MutiplesCambiosDeNivel() {
-        Nemo nemo = new Nemo();
-        nemo.indications ( "dddddduuuu" ); // baja 6 sube 4
+        Nemo nemo = createSubmarineWithCommand("dddddduuuu");
         compareDepths(nemo, 2);
         compareCapacityOfLaunch(nemo, false); // pensar si sacarlo o no
     }
 
     @Test
     public void test07RotarDerecha() {
-        Nemo nemo = new Nemo();
-        nemo.indications ( "r" );
-        compareDirections(nemo, 0, 1);
+        compareDirections(createSubmarineWithCommand("r"), 0, 1);
     }
 
     @Test
     public void test08RotarIzquierda() {
-        Nemo nemo = new Nemo();
-        nemo.indications ( "l" );
-        compareDirections(nemo, 0, -1);
+        compareDirections(createSubmarineWithCommand("l"), 0, -1);
     }
 
     @Test
     public void test09Girar90grados(){
-        Nemo nemo = new Nemo();
-        nemo.indications("ll"); // gira90grados
-        compareDirections(nemo, -1, 0);
+        compareDirections(createSubmarineWithCommand("ll"), -1, 0);
     }
 
     @Test
@@ -82,54 +69,47 @@ public class Nemotest {
 
     @Test
     public void test11AvanzarNorth() {
-        Nemo nemo = new Nemo();
-        nemo.indications ( "f" );
+        Nemo nemo = createSubmarineWithCommand("f");
         compareCoords(nemo, 1, 0);
         compareDirections(nemo, 1, 0);
     }
 
     @Test
     public void test12AvanzarWest() {
-        Nemo nemo = new Nemo();
-        nemo.indications ( "rf" );
+        Nemo nemo = createSubmarineWithCommand("rf");
         compareCoords(nemo, 0, 1);
         compareDirections(nemo, 0,1);
     }
 
     @Test
     public void test13AvanzarEast() {
-        Nemo nemo = new Nemo();
-        nemo.indications ( "rrf" );
+        Nemo nemo = createSubmarineWithCommand( "rrf" );
         compareState( nemo, 0,true, -1, 0,-1, 0);
     }
 
     @Test
     public void test14AvanzarSouth() {
-        Nemo nemo = new Nemo();
-        nemo.indications ( "lf" );
+        Nemo nemo = createSubmarineWithCommand("lf");
         compareState( nemo, 0,true, 0, -1,0, -1);
     }
 
     @Test
     public void test15TirarCapsulaEnSuperficie() {
-        Nemo nemo = new Nemo();
-        nemo.indications( "m" );
+        Nemo nemo = createSubmarineWithCommand("m");
         compareDepths(nemo, 0);
         compareCapacityOfLaunch(nemo, true);
     }
 
     @Test
     public void test16TirarCapsulaEnBelowSurface() {
-        Nemo nemo = new Nemo();
-        nemo.indications( "d m" );
+        Nemo nemo = createSubmarineWithCommand("d m");
         compareDepths(nemo, 1);
         compareCapacityOfLaunch(nemo, true);
     }
 
     @Test
     public void test17TirarCapsulaEnDeep() {
-        Nemo nemo = new Nemo();
-        nemo.indications( "d d" );
+        Nemo nemo = createSubmarineWithCommand("d d");
         compareDepths(nemo, 2);
         compareCapacityOfLaunch(nemo, false);
         assertThrowsLike ( ()->nemo.indications("m") , Deep.NoSePuedeLiberarLaCapsula );
@@ -137,8 +117,7 @@ public class Nemotest {
 
     @Test
     public void test18DescenderyAvanzar() {
-        Nemo nemo = new Nemo();
-        nemo.indications( "df" );
+        Nemo nemo = createSubmarineWithCommand("df");
         compareDepths(nemo, 1);
         //compareCapacityOfLaunch(nemo, true);  para mi no hace falta
         compareCoords(nemo, 1, 0);
@@ -146,16 +125,15 @@ public class Nemotest {
 
     @Test
     public void test19DescenderGirarYAvanzar(){
-        Nemo nemo = new Nemo();
-        nemo.indications( "drf" );
+        Nemo nemo = createSubmarineWithCommand("drf");
         compareDepths(nemo, 1);
         compareCapacityOfLaunch(nemo, true);
         compareCoords(nemo, 0, 1);
         compareDirections(nemo, 0, 1);
     }
 
-    private static void compareState( Nemo nemo, int depth, boolean canLaunch,int xcoord, int ycoord, int xdire, int ydire) {
-       compareDepths(nemo, depth);
+    private static void compareState( Nemo nemo,  int depth, boolean canLaunch,int xcoord, int ycoord, int xdire, int ydire) {
+        compareDepths(nemo, depth);
        compareCapacityOfLaunch(nemo, canLaunch);
        compareCoords(nemo, xcoord, ycoord);
        compareDirections(nemo, xdire, ydire);
@@ -179,7 +157,7 @@ public class Nemotest {
         assertEquals ( message , assertThrows ( Exception.class , executable ) .getMessage() );
     }
 
-    private Nemo createSubmarineWithCommand(String command){ ///// CHECK!!!!!
+    private Nemo createSubmarineWithCommand(String command){
         Nemo nemo = new Nemo();
         nemo.indications(command);
         return nemo;
