3e1c870c43b63717cb24b84f932c627e8ef82b05
Eliana Ostro <elianaostro@gmail.com>
2023-10-25 10:18:41 -0300

    TP4

---
+++ TP4/src/linea/Linea.java
@@ -0,0 +1,21 @@
+package linea;
+
+public class Linea {
+    public Linea(int prompt, int prompt1, char c) {
+    }
+
+    public String show() {
+        return null;
+    }
+
+    public boolean finished() {
+        return false;
+    }
+
+    public void playRedkAt(int prompt) {
+    }
+
+    public void playBlueAt(int prompt) {
+    }
+}
+
