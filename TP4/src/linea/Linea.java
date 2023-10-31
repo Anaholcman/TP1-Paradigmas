1693bd9de9323ed73558695227d29d6af73cc6da
Anaholcman <anitaholcman@gmail.com>
2023-10-31 17:02:24 -0300

    first steps

--- TP4/src/linea/Linea.java
+++ TP4/src/linea/Linea.java
@@ -1,21 +1,77 @@
 package linea;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Linea {
-    public Linea(int prompt, int prompt1, char c) {
+
+    private char turno;
+    private int base;
+    private int altura;
+    private char TipoDeJuego;
+    private ArrayList<List> tablero;
+
+    public  Linea (int base, int altura, char tipoDeJuego) {
+        turno = 'R';
+        this.base = base;
+        this.altura = altura;
+        TipoDeJuego = tipoDeJuego;
+        tablero = new ArrayList<>();
+        if (altura < 4 || base < 4){
+            throw new RuntimeException("No se puede hacer un tablero menor a 4");
+        }
+        for (int i = 0; i < base; i++){
+            tablero.add( new ArrayList<>(altura) );
+            for (int j = 0; j < altura; j++){
+                tablero.get(i).add('-');
+            }
+        }
+
+
+    }
+
+    public boolean turnoRojas() {
+        return turno == 'R';
+    }
+
+    public boolean turnoAzules() {
+        return turno == 'A';
     }
 
     public String show() {
-        return null;
+        String tableroString = "";
+        for (int i = altura; i > 0; i--){
+            for (int j = 0; j < base; j++){
+                tableroString += tablero.get(j).get(i);
+            }
+            tableroString += "\n";
+        }
+        return tableroString;
+
     }
 
     public boolean finished() {
         return false;
     }
 
-    public void playRedkAt(int prompt) {
+    public void playRedkAt(int prompt)  {
+        if (turno != 'R'){
+            throw  new RuntimeException("No es tu turno");
+        }
+        else {
+            turno = 'A';
+        }
     }
 
     public void playBlueAt(int prompt) {
+        if (turno != 'A'){
+            throw  new RuntimeException("No es tu turno");
+        }
+        else {
+            turno = 'R';
+        }
     }
+
+
 }
 
--- TP4/src/linea/LineaTest.java
+++ TP4/src/linea/LineaTest.java
@@ -2,13 +2,37 @@ package linea;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LineaTest {
-    @Test public void test00(){
-        assertTrue(true);
+
+    @Test public void testTableroEmpiezaVacio(){
+        Linea game = new Linea( 4, 4,'C' );
+        assertTrue(game.show().equals("----\n----\n----\n----\n"));
+
     }
 
-    //holaaa probando
+    @Test public void testNoSePuedeHacer3x3(){
+        assertThrows(RuntimeException.class, () -> new Linea( 3, 3,'C' ));
+    }
+    @Test public void testEmpiezanLasRojas(){
+        Linea game = new Linea( 4, 4,'C' );
+        assertTrue(game.turnoRojas());
+    }
+
+    @Test public void testJueganLasAzules(){
+        Linea game = new Linea( 4, 4,'C' );
+        game.playRedkAt( 1 );
+        assertTrue(game.turnoAzules());
+        assertFalse(game.turnoRojas());
+
+    }
+
+    @Test public void testJueganLasRojas(){
+        Linea game = new Linea( 4, 4,'C' );
+        game.playRedkAt( 1 );
+        game.playBlueAt( 1 );
+        assertTrue(game.show().equals("----\n----\n----\n----\n"));
+
+    }
 }
